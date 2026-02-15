29f04919807157c381d35f3668dc16911f0a445f
Cindrella <shaikmarwaan361@gmail.com>
2026-02-15 14:34:39 -0800

    FadeIn Remove

--- app/notes/[note]/page.tsx
+++ app/notes/[note]/page.tsx
@@ -1,5 +1,4 @@
 import { getNote } from "@/app/actions/note";
-import FadeIn from "@/components/fadeIn";
 import { Rose } from "lucide-react";
 import Link from "next/link";
 
@@ -23,14 +22,12 @@ export default async function Note({
       </div>
     );
   return (
-    <FadeIn>
-      <div className="md:p-30 p-7 font-secondary flex flex-col gap-5 md:h-dvh md:overflow-y-auto no-scrollbar md:pb-45 pb-45">
-        <p className="text-xs text-muted-foreground font-sans">{date}</p>
-        <h2 className="md:text-4xl font-charter text-3xl">{title}</h2>
-        <p className="leading-6.5 text-neutral-300/90 font-sans tracking-wide whitespace-pre-wrap">
-          {content}
-        </p>
-      </div>
-    </FadeIn>
+    <div className="md:p-30 p-7 font-secondary flex flex-col gap-5 md:h-dvh md:overflow-y-auto no-scrollbar md:pb-45 pb-45">
+      <p className="text-xs text-muted-foreground font-sans">{date}</p>
+      <h2 className="md:text-4xl font-charter text-3xl">{title}</h2>
+      <p className="leading-6.5 text-neutral-300/90 font-sans tracking-wide whitespace-pre-wrap">
+        {content}
+      </p>
+    </div>
   );
 }
