feb1adaf37be8326c7346fce255c056f4d7ef7e4
Cindrella <shaikmarwaan361@gmail.com>
2026-02-17 13:10:58 -0800

    Refactor core logic for significant performance improvements

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,6 +1,7 @@
 import { nanoid } from "nanoid/non-secure";
 import { redirect } from "next/navigation";
 
+// TODO
 export default function NewPage() {
   const uid = nanoid();
   redirect(`/new/${uid}`);
