d671644ce6130bf96f3d771d3b202ec81d6c832b
Cindrella <shaikmarwaan361@gmail.com>
2026-02-21 16:22:08 +0530

    #

--- components/ui/notes_container.tsx
+++ components/ui/notes_container.tsx
@@ -16,7 +16,7 @@ export default function NotesContainer({ fetch_notes }: any) {
       className={`flex-col md:gap-7 gap-15 mt-0 ${hide ? "hidden md:flex" : "flex"}`}
     >
       {fetch_notes?.map((elem: any, index: number) => (
-        <Note actie={elem.uid === active} note={elem} key={index} />
+        <Note active={elem.uid === active} note={elem} key={index} />
       ))}
       {fetch_notes.length === 0 && (
         <div className="mx-auto md:my-50 my-40 flex flex-col justify-center items-center gap-3.5">
